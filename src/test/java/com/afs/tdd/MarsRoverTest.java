624c52507005e92a376d2287d0926c83d8f56333
BIN <BIN.JIAN@OOCL.COM>
2025-09-05 15:51:47 +0800

    test: add case for move

---
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -0,0 +1,20 @@
+package com.afs.tdd;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class MarsRoverTest {
+  @Test
+  void should_move_when_executeCommand_given_M() {
+    //Given
+    Location location = new Location(0,0,Direction.N);
+    MarsRover marsRover = new MarsRover(location);
+    //When
+    marsRover.executeCommand(Command.M);
+    //Then
+    Location currentLocation = new Location(0,1,Direction.N);
+    assertEquals(currentLocation.getLocaltionY(), marsRover.getLocation().getLocationY());
+
+  }
+}
