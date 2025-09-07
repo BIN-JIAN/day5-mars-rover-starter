6482056044ba09b7bb75a75390922bcfa76f079d
BIN <BIN.JIAN@OOCL.COM>
2025-09-07 13:40:00 +0800

    test: add test for M direction E

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -5,10 +5,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 class MarsRoverTest {
+
   @Test
-  void should_move_direction_N_when_executeCommand_given_1() {
+  void should_move_direction_N_when_executeCommand_given_01() {
     //Given
-    Location location = new Location(0,0,Direction.N);
+    Location location = new Location(0, 0, Direction.N);
     MarsRover marsRover = new MarsRover(location);
     //When
     marsRover.executeCommand(Command.M);
@@ -16,15 +17,26 @@ class MarsRoverTest {
     assertEquals(1, marsRover.getLocation().getLocationY());
 
   }
+
   @Test
-  void should_move_direction_S_when_executeCommand_given_minus1() {
+  void should_move_direction_S_when_executeCommand_given_0minus1() {
     //Given
-    Location location = new Location(0,0,Direction.S);
+    Location location = new Location(0, 0, Direction.S);
     MarsRover marsRover = new MarsRover(location);
     //When
     marsRover.executeCommand(Command.M);
     //Then
     assertEquals(-1, marsRover.getLocation().getLocationY());
+  }
 
+  @Test
+  void should_move_direction_E_when_executeCommand_given_10() {
+    //Given
+    Location location = new Location(0, 0, Direction.E);
+    MarsRover marsRover = new MarsRover(location);
+    //When
+    marsRover.executeCommand(Command.M);
+    //Then
+    assertEquals(1, marsRover.getLocation().getLocationX());
   }
 }
